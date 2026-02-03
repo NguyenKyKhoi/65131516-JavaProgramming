6999d895f3d6d2fbcdc1c288e7ee009c58a540b2
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-02-03 23:23:59 +0700

    Thêm mã nhập, khai báo biến

--- Java/BaiTap1_NhapXuat_TinhTong2So/src/tiil/edu/TongHaiSo.java
+++ Java/BaiTap1_NhapXuat_TinhTong2So/src/tiil/edu/TongHaiSo.java
@@ -1,9 +1,22 @@
 package tiil.edu;
 
+import java.util.Scanner;
+
 public class TongHaiSo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Khai báo
+		double num1, num2, sum;
+
+		// Khai báo đối tượng Scanner
+		Scanner banPhim = new Scanner(System.in);
+
+		// In ra thông tin, mời User nhập số thứ nhất
+		System.out.print("Mời nhập số thứ nhất: ");
+		num1 = banPhim.nextDouble();
+
+		System.out.print("Mời nhập số thứ hai: ");
+		num2 = banPhim.nextDouble();
 
 	}
 
