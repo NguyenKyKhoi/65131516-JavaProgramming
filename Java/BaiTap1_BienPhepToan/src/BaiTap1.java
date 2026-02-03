3358521f451adcfa1f45b0e4dadec1db4d2902df
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-02-03 22:43:37 +0700

    Hoàn thiện bài 1 biến và toán tử

--- Java/BaiTap1_BienPhepToan/src/BaiTap1.java
+++ Java/BaiTap1_BienPhepToan/src/BaiTap1.java
@@ -3,14 +3,17 @@ public class BaiTap1 {
 
 	public static void main(String[] args) {
 		// 1. khai báo các biến
-		int a = 15;
-		int b = 4;
+		double a = 15;
+		double b = 4;
 		double c = 3.5;
 
 		// 2. Tính toán
-		int tong = a + b;
-		double thuong = a / c;
+		double tong = a + b;
+		double thuong = a / b;
 
+		// 3. In kết quả
+		System.out.println("Tổng là: " + tong);
+		System.out.println("Thương là: " + thuong + "\n");
 	}
 
 }
