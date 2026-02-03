6062ba316e6958119a9eac39dc74f44fe7dc8843
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-02-03 23:38:08 +0700

    updated

--- Java/BaiTap1_NhapXuat_TinhTong2So/src/tiil/edu/TongHaiSo.java
+++ Java/BaiTap1_NhapXuat_TinhTong2So/src/tiil/edu/TongHaiSo.java
@@ -18,6 +18,16 @@ public class TongHaiSo {
 		System.out.print("Mời nhập số thứ hai: ");
 		num2 = banPhim.nextDouble();
 
+		// Tính toán
+		sum = num1 + num2;
+
+		// Xuất ra print không hiểu kiểu num1, num2, sum 1 lần luôn nên ta dùng printf
+		// như C
+		System.out.print("Tổng của " + num1 + " và " + num2 + " là " + sum);
+
+		// cách 2 hay dùng
+		System.out.printf("\nTong cua %.2f và %.2f là %.2f", num1, num2, sum);
+
 	}
 
 }
