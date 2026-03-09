80745ec0fa9263034c8b823a3114d7e11b6dadd7
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-03-09 13:37:04 +0700

    updated

---
+++ Ex_Circel_1_2/src/tiil/edu/Circle.java
@@ -0,0 +1,59 @@
+package tiil.edu;
+
+public class Circle {
+	private double radius;
+	private String color;
+
+	public Circle() {
+		radius = 1.0;
+		color = "red";
+	}
+
+	public Circle(double r) {
+		radius = r;
+		color = "red";
+	}
+
+	public Circle(double r, String c) {
+		radius = r;
+		color = c;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public double getArea() {
+		return radius * radius * Math.PI;
+	}
+
+	public double getCircumference() {
+		return radius * 2 * 3.14;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setRadius(double newRadius) {
+		radius = newRadius;
+	}
+
+	public void setColor(String newColor) {
+		color = newColor;
+	}
+
+	// nên dùng
+//	public Circle(double radius) {
+//		this.radius = radius;
+//	}
+//	
+//	public void setRadius(double radius) {
+//		this.radius = radius;
+//	}
+
+	// phương trức toString();
+	public String toString() {
+		return "Circle[radius = " + radius + " color = " + color + "]";
+	}
+}
