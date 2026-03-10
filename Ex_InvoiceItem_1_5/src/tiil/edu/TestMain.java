624da4089612d444769751aedecf39cf24254746
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-03-10 21:16:01 +0700

    updated

---
+++ Ex_InvoiceItem_1_5/src/tiil/edu/TestMain.java
@@ -0,0 +1,19 @@
+package tiil.edu;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		InvoiceItem inv1 = new InvoiceItem("A101", "Bút đỏ", 888, 0.08);
+		System.out.println(inv1);
+		inv1.setQty(999);
+		inv1.setUnitPrice(0.99);
+		System.out.println(inv1);
+		System.out.println("id là: " + inv1.getId());
+		System.out.println("mô tả là: " + inv1.getDesc());
+		System.out.println("số lượng là: " + inv1.getQty());
+		System.out.println("giá đơn vị là: " + inv1.getUnitPrice());
+		System.out.println("Tổng cộng là: " + inv1.getTotal());
+
+	}
+
+}
