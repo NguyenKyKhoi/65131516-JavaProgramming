2be424d24704444542eb056d4bcfc554319cd6de
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-03-10 20:00:29 +0700

    Employee 1_4

--- Ex_Employee_1_4/src/tiil/edu/Employee.java
+++ Ex_Employee_1_4/src/tiil/edu/Employee.java
@@ -1,5 +1,53 @@
 package tiil.edu;
 
 public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getName() {
+		return this.firstName + " " + this.lastName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Employee(int id, String firstName, String lastName, int salary) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+
+	public int getAnnualSalary() {
+		return salary * 12;
+	}
+
+	public int raiseSalary(int percent) {
+		return salary += salary * percent / 100;
+	}
+
+	public String toString() {
+		String s = "Employee[id =  " + id + " name = " + getName() + ", salary = " + salary + "]";
+		return s;
+	}
 
 }
