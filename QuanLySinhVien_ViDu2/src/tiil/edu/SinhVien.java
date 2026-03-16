3144ebe8bbaf257fcd8d31087e65325e4fcb7fed
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-03-16 13:48:08 +0700

    getter, setter,  SinhVIen

--- QuanLySinhVien_ViDu2/src/tiil/edu/SinhVien.java
+++ QuanLySinhVien_ViDu2/src/tiil/edu/SinhVien.java
@@ -1,5 +1,29 @@
 package tiil.edu;
 
 public class SinhVien {
+	private String MaSV;
+	private String hoTen;
+	private double diemTB;
 
+	public SinhVien(String MaSV, String hoTen, double diemTB) {
+		this.MaSV = MaSV;
+		this.hoTen = hoTen;
+		this.diemTB = diemTB;
+	}
+
+	public String getMaSV() {
+		return MaSV;
+	}
+
+	public String getHoTen() {
+		return hoTen;
+	}
+
+	public double getDiemTB() {
+		return diemTB;
+	}
+
+	public String toString() {
+		return String.format("Mã SV: %s | Họ tên: %s | Điểm TB: %.2f", MaSV, hoTen, diemTB);
+	}
 }
