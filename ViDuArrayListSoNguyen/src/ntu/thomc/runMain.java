2e9b272503e13571f778bf6e6c84006f7ae916c9
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-03-16 08:01:54 +0700

    In các phần tử trong mảng

--- ViDuArrayListSoNguyen/src/ntu/thomc/runMain.java
+++ ViDuArrayListSoNguyen/src/ntu/thomc/runMain.java
@@ -1,9 +1,26 @@
 package ntu.thomc;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class runMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
+
+		Scanner banPhim = new Scanner(System.in);
+
+		for (int i = 0; i < 10; i++) {
+			System.out.print("a[" + i + "] = ");
+			Integer temp = banPhim.nextInt();
+			dsSoNguyen.add(temp);
+		}
+
+		System.out.println("In các phần tử trong mảng : ");
+		for (int i = 0; i < dsSoNguyen.size(); i++) {
+			Integer temp = dsSoNguyen.get(i);
+			System.out.print(temp + " ");
+		}
 
 	}
 
