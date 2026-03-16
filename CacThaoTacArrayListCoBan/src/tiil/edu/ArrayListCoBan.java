4b5b42d8ceb80b5ae69165932be91687c0736dc8
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-03-16 09:42:21 +0700

    Thêm phần tử

--- CacThaoTacArrayListCoBan/src/tiil/edu/ArrayListCoBan.java
+++ CacThaoTacArrayListCoBan/src/tiil/edu/ArrayListCoBan.java
@@ -1,9 +1,21 @@
 package tiil.edu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListCoBan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("=== ARRAYLIST CƠ BẢN - MINH HỌA CÁC PHƯƠNG THỨC ===\n");
+
+		// 1. Khai báo ArrayList với Generics
+		List<String> danhSachTen = new ArrayList<>();
+
+		// 2. Thêm phần tử - add()
+		System.out.println("--- THÊM PHẦN TỬ ---");
+		danhSachTen.add("Nguyễn Văn A");
+		danhSachTen.add("Trần Thị B");
+		danhSachTen.add("Lê Văn C");
 
 	}
 
