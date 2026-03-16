879a3a9fa0166137788f9b72cae6747f305e0f7e
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-03-16 22:41:48 +0700

    các lựa chọn và chức năng

--- QuanLySanPham/src/tiil/com/MainQuanLySanPham.java
+++ QuanLySanPham/src/tiil/com/MainQuanLySanPham.java
@@ -1,10 +1,95 @@
 package tiil.com;
 
+import java.util.Scanner;
+
 public class MainQuanLySanPham {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		QuanLySanPham ql = new QuanLySanPham();
+		int luaChon = 0;
 
+		ql.themSP(new SanPham("SP01", "Coca Cola", "15000", "50"));
+		ql.themSP(new SanPham("SP02", "Banh Mi", "20000", "5"));
+		ql.themSP(new SanPham("SP03", "Sua Tuoi", "12000", "8"));
+
+		do {
+			System.out.println("\n---------- MENU QUẢN LÝ SẢN PHẨM ----------");
+			System.out.println("1. Thêm sản phẩm mới");
+			System.out.println("2. Xóa sản phẩm theo mã");
+			System.out.println("3. Tìm sản phẩm theo tên");
+			System.out.println("4. Sắp xếp sản phẩm theo giá tăng dần");
+			System.out.println("5. Sắp xếp sản phẩm theo giá giảm dần");
+			System.out.println("6. Tính tổng giá trị kho hàng");
+			System.out.println("7. Liệt kê sản phẩm sắp hết hàng (< 10)");
+			System.out.println("8. Hiển thị tất cả sản phẩm");
+			System.out.println("0. Thoát chương trình");
+			System.out.print("Mời bạn chọn (0-8): ");
+			luaChon = sc.nextInt();
+			sc.nextLine(); // Đọc bỏ dòng trống sau khi nhập số
+
+			switch (luaChon) {
+			case 1:
+				System.out.print("Nhập mã SP: ");
+				String ma = sc.nextLine();
+				System.out.print("Nhập tên SP: ");
+				String ten = sc.nextLine();
+				System.out.print("Nhập giá: ");
+				double gia = sc.nextDouble();
+				System.out.print("Nhập số lượng: ");
+				int sl = sc.nextInt();
+				ql.themSP(new SanPham(ma, ten, gia, sl));
+				break;
+
+			case 2:
+				System.out.print("Nhập mã SP cần xóa: ");
+				String maXoa = sc.nextLine();
+				ql.timVaXoa(maXoa);
+				break;
+
+			case 3:
+				System.out.print("Nhập tên SP cần tìm: ");
+				String tenTim = sc.nextLine();
+				ql.timTheoTen(tenTim);
+				break;
+
+			case 4:
+				ql.sapXepTang();
+				System.out.println("Đã sắp xếp tăng dần theo giá.");
+				break;
+
+			case 5:
+				ql.sapXepGiam();
+				System.out.println("Đã sắp xếp giảm dần theo giá.");
+				break;
+
+			case 6:
+				System.out.println("Tổng giá trị kho hàng là: " + ql.tongGiaTriKhoHang());
+				break;
+
+			case 7:
+				System.out.println("Các sản phẩm sắp hết hàng:");
+				ql.sanPhamSapHetHang();
+				break;
+
+			case 8:
+				// Vì QuanLySanPham chưa có hàm in hết, tôi gọi tạm ở đây để bạn test
+				System.out.println("Danh sách hiện tại:");
+				// Bạn có thể thêm phương thức xuatTatCa vào QuanLySanPham
+				// Ở đây tôi dùng tạm loop để demo
+				ql.timTheoTen(""); // Một mẹo nhỏ nếu hàm tìm kiếm của bạn hỗ trợ chuỗi rỗng
+				break;
+
+			case 0:
+				System.out.println("Tạm biệt!");
+				break;
+
+			default:
+				System.out.println("Lựa chọn không hợp lệ!");
+			}
+		} while (luaChon != 0);
+
+		sc.close();
 	}
 
 }
--- QuanLySanPham/src/tiil/com/QuanLySanPham.java
+++ QuanLySanPham/src/tiil/com/QuanLySanPham.java
@@ -16,4 +16,71 @@ public class QuanLySanPham {
 		System.out.println("Sản Phẩm đã thêm : " + sp.getTenSP());
 	}
 
+	// xóa sản phẩm theo mã (tìm sản phẩm rồi thực hiện xóa)
+	public boolean timVaXoa(String maSP) {
+		for (int i = 0; i < danhSachSP.size(); i++) {
+			if (danhSachSP.get(i).getMaSP().equals(maSP)) {
+				SanPham spBiXoa = danhSachSP.remove(i);
+				System.out.println("Sản phẩm đã xóa : " + spBiXoa.getTenSP());
+				return true;
+			}
+		}
+		System.out.println("Không tìm thấy sản phẩm với mã : " + maSP);
+		return false;
+	}
+
+	// tìm sản phẩm theo tên
+	public void timTheoTen(String tenCanTim) {
+		for (SanPham sp : danhSachSP) {
+			if (sp.getTenSP().equalsIgnoreCase(tenCanTim)) {
+				System.out.println("Kết quả tìm kiếm : " + sp.toString());
+				return;
+			}
+		}
+		System.out.println("Rất tiếc, không tìm thấy sản phẩm: " + tenCanTim);
+	}
+
+	// sắp xếp sản phẩm theo giá (giảm dần)
+	public void sapXepGiam() {
+		for (int i = 0; i < danhSachSP.size() - 1; i++) {
+			for (int j = 0; j < danhSachSP.size() - 1 - i; j++) {
+				if (danhSachSP.get(j).getGia() < danhSachSP.get(j + 1).getGia()) {
+					SanPham temp = danhSachSP.get(j);
+					danhSachSP.set(j, danhSachSP.get(j + 1));
+					danhSachSP.set(j + 1, temp);
+				}
+			}
+		}
+	}
+
+	// sắp xếp sản phẩm theo (tăng dần)
+	public void sapXepTang() {
+		for (int i = 0; i < danhSachSP.size() - 1; i++) {
+			for (int j = 0; j < danhSachSP.size() - 1 - i; j++) {
+				if (danhSachSP.get(j).getGia() > danhSachSP.get(j + 1).getGia()) {
+					SanPham temp = danhSachSP.get(j);
+					danhSachSP.set(j, danhSachSP.get(j + 1));
+					danhSachSP.set(j + 1, temp);
+				}
+			}
+		}
+	}
+
+	// Tính tổng giá trị kho hàng
+	public double tongGiaTriKhoHang() {
+		double sum = 0;
+		for (SanPham sp : danhSachSP) {
+			sum += sp.getGia() * sp.getSoLuong();
+		}
+		return sum;
+	}
+
+	// Liệt kê sản phẩm sắp hết hàng (số lượng < 10)
+	public void sanPhamSapHetHang() {
+		for (SanPham x : danhSachSP) {
+			if (x.getSoLuong() < 10) {
+				System.out.println(x.getTenSP());
+			}
+		}
+	}
 }
