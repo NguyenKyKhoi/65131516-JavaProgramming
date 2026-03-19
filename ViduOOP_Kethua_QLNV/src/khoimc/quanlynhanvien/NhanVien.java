8f657b407f2e98113d54125128b4299684edc926
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-03-19 08:28:58 +0700

    toString

--- ViduOOP_Kethua_QLNV/src/khoimc/quanlynhanvien/NhanVien.java
+++ ViduOOP_Kethua_QLNV/src/khoimc/quanlynhanvien/NhanVien.java
@@ -1,5 +1,43 @@
 package khoimc.quanlynhanvien;
 
 public class NhanVien {
+	// thuoc tinh
+	private String maNV;
+	private String hoTen;
+	private double luongCoBan;
+	// Ham tao
+//	public NhanVien() {
+//		
+//	}
+
+	public NhanVien(String maNV, String hoTen, double luongCoBan) {
+		this.maNV = maNV;
+		this.hoTen = hoTen;
+		this.luongCoBan = luongCoBan;
+	}
+
+	public NhanVien() {
+		super();
+	}
+
+	public double tinhLuong() {
+		return luongCoBan;
+	}
+
+	@Override
+	public String toString() {
+		String chuoiXuat = "";
+		chuoiXuat = chuoiXuat + "Ma NV: " + this.maNV;
+		chuoiXuat = chuoiXuat + "\nHo ten: " + this.hoTen;
+		chuoiXuat = chuoiXuat + "\nLuong co ban: " + this.luongCoBan;
+		return chuoiXuat;
+//		return "NhanVien [maNV=" + maNV + ", hoTen=" + hoTen + ", luongCoBan=" + luongCoBan + "]";
+	}
+
+	public void hienThiThongTin() {
+		System.out.println("Ma NV: " + maNV);
+		System.out.println("Ho va Ten: " + hoTen);
+		System.out.println("Luong co ban: " + luongCoBan);
+	}
 
 }
---
+++ ViduOOP_Kethua_QLNV/src/khoimc/quanlynhanvien/Run.java
@@ -0,0 +1,14 @@
+package khoimc.quanlynhanvien;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NhanVien nv1 = new NhanVien("001", "Mai Cuong Tho", 2300000);
+		// nv1.hienThiThongTin();
+
+		System.out.println(nv1.toString());
+
+	}
+
+}
