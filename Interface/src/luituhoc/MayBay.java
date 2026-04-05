7f616c14453e81553ea3dbe4dd37e3d82aeff54d
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-04-05 09:04:36 +0700

    Máy Bay

--- Interface/src/luituhoc/MayBay.java
+++ Interface/src/luituhoc/MayBay.java
@@ -1,5 +1,29 @@
 package luituhoc;
 
-public class MayBay {
+class MayBay implements CoTheBay {
+	private String ten;
+	private String hangSanXuat;
+
+	public MayBay(String ten, String hangSanXuat) {
+		super();
+		this.ten = ten;
+		this.hangSanXuat = hangSanXuat;
+	}
+
+	@Override
+	public void bay() {
+		System.out.println(ten + " Đang bay trên bầu trời");
+	}
+
+	// có thể override phương thức mặc định
+	@Override
+	public void haCanh() {
+		System.out.println(ten + " đang hạ cánh tại sân bay...");
+	}
+
+	@Override
+	public String toString() {
+		return "MayBay [ten=" + ten + ", hangSanXuat=" + hangSanXuat + ", soCanh= " + CoTheBay.SO_CANH + "]";
+	}
 
 }
--- Interface/src/luituhoc/Vit.java
+++ Interface/src/luituhoc/Vit.java
@@ -1,5 +1,42 @@
 package luituhoc;
 
-public class Vit {
+class Vit extends DongVat implements CoTheBay, CoTheChay, CoTheBoi {
+	public Vit(String ten, int tuoi) {
+		super(ten, tuoi);
+	}
 
+	@Override
+	public void an() {
+		System.out.println("Đang mổ thóc...");
+	}
+
+	@Override
+	public void bay() {
+		System.out.println(ten + " Đang bay thấp...");
+	}
+
+	@Override
+	public void boi() {
+		System.out.println(ten + " Đang bơi trên mặt nước...");
+	}
+
+	@Override
+	public void chay() {
+		System.out.println(ten + " Đang chạy lạch bạch...");
+	}
+
+	@Override
+	public void chay(double tocDo) {
+		System.out.println(ten + " Đang chạy với tốc độ " + tocDo + " km/h");
+	}
+
+	// Có thể sử dụng phương thức mặc định từ interface
+	public void hienThiTatCaKhNang() {
+		System.out.println("\n" + ten + " có thể:");
+		bay();
+		boi();
+		chay();
+		haCanh(); // Từ CoTheBay interface
+		nghiNgoi(); // Từ CoTheChay interface
+	}
 }
