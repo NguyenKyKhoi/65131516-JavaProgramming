b8f6769c8cd2dbab60bee20d1260ec1198f12e48
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-04-19 08:17:23 +0700

    SinhVien

---
+++ ViDuDocStream/src/MainObject.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class MainObject {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		FileReader fR = new FileReader("object_2");
+		BufferedReader bufReader = new BufferedReader(fR);
+		ArrayList<Object> dsOject = new ArrayList<Object>();
+
+	}
+
+}
