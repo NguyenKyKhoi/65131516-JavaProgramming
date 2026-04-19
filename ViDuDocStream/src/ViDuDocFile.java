a9f17d9ca213fbb8cadbd22c90122122d5ff0b43
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-04-19 07:54:21 +0700

    object

--- ViDuDocStream/src/ViDuDocFile.java
+++ ViDuDocStream/src/ViDuDocFile.java
@@ -6,12 +6,16 @@ public class ViDuDocFile {
 	public static void main(String[] args) throws IOException {
 		FileReader fR = new FileReader("Input.txt");
 		BufferedReader bufReader = new BufferedReader(fR);
+		String line = bufReader.readLine();
+//		String line1 = bufReader.readLine();
+//		Double so1 = Double.parseDouble(line1);
+//
+//		String line2 = bufReader.readLine();
+//		Double so2 = Double.parseDouble(line2);
 
-		String line1 = bufReader.readLine();
-		Double so1 = Double.parseDouble(line1);
-
-		String line2 = bufReader.readLine();
-		Double so2 = Double.parseDouble(line2);
+		String[] parts = line.split("\\s+");
+		double so1 = Double.parseDouble(parts[0]);
+		double so2 = Double.parseDouble(parts[1]);
 
 		double tong = so1 + so2;
 		System.out.println("Tổng là: " + String.valueOf(tong));
