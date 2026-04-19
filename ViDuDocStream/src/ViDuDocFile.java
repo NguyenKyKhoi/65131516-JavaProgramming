fdf5dccbd817ef701bb55afe9c2e58575fc157a8
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-04-19 07:31:30 +0700

    sum

--- ViDuDocStream/src/ViDuDocFile.java
+++ ViDuDocStream/src/ViDuDocFile.java
@@ -1,4 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ViDuDocFile {
+	public static void main(String[] args) throws IOException {
+		FileReader fR = new FileReader("Input.txt");
+		BufferedReader bufReader = new BufferedReader(fR);
 
+		String line1 = bufReader.readLine();
+		Double so1 = Double.parseDouble(line1);
+
+		String line2 = bufReader.readLine();
+		Double so2 = Double.parseDouble(line2);
+
+		double tong = so1 + so2;
+		System.out.println("Tổng là: " + String.valueOf(tong));
+	}
 }
