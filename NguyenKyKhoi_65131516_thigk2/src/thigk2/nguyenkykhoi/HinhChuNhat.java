366b2f2508b59d40226fd6ee777d10596b0ba73f
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-04-23 07:59:33 +0700

    Lần thứ 3

---
+++ NguyenKyKhoi_65131516_thigk2/src/thigk2/nguyenkykhoi/HinhChuNhat.java
@@ -0,0 +1,10 @@
+package thigk2.nguyenkykhoi;
+
+public class HinhChuNhat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- NguyenKyKhoi_65131516_thigk2/src/thigk2/nguyenkykhoi/MainSanPham.java
+++ NguyenKyKhoi_65131516_thigk2/src/thigk2/nguyenkykhoi/MainSanPham.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 //1. tạo danh sách ArrayList dùng foreach kết hợp to String để xuất
+//2. thêm mới nhập từ bàn phím rồi dùng add thêm vào ds sản phẩm
 
 public class MainSanPham {
 
@@ -15,13 +16,41 @@ public class MainSanPham {
 		ArrayList<SanPham> dsSanPham = new ArrayList<SanPham>();
 
 		dsSanPham.add(new SanPham("SP001", "Laptop Gaming G15", "Điện tử", 22500000));
-		dsSanPham.add(new SanPham("SP002", "Chuột không dây M331", "Phụ kiện", 350000));
+		dsSanPham.add(new SanPham("SP002", "Thuốc bổ máu", "Thực phẩm chức năng", 350000));
 		dsSanPham.add(new SanPham("SP003", "Bàn phím cơ K87", "Phụ kiện", 1200000));
 
 		for (var temp : dsSanPham) {
 			System.out.println(temp.toString());
 		}
 
+		// thêm mới vào ds, thông tin nhập từ bàn phím
+		System.out.println("Thêm mới 1 sản phẩm : ");
+		System.out.print("Nhập mã sản phẩm : ");
+		String masp = sc.nextLine();
+		System.out.print("Nhập tên sản phẩm : ");
+		String tensp = sc.nextLine();
+		System.out.print("Nhập loại sản phẩm : ");
+		String loaisp = sc.nextLine();
+		System.out.print("Nhập giá sản phẩm : ");
+		double giasp = sc.nextDouble();
+
+		SanPham temp = new SanPham(masp, tensp, loaisp, giasp);
+		dsSanPham.add(temp);
+
+		// in thông tin
+		for (var x : dsSanPham) {
+			System.out.println(x.toString());
+		}
+
+		// in lại ra màn hình với loại là thực phẩm chức năng
+		System.out.println("Loại sản phẩm là thực phẩm chức năng : ");
+		for (var y : dsSanPham) {
+			if (y.getLoaiSP() == "Thực phẩm chức năng") {
+				System.out.print(y.toString());
+			}
+		}
+
+		sc.close();
 	}
 
 }
