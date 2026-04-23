a355b79b73a682d4209fb1c34ae514aa555d4af4
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-04-23 07:49:18 +0700

    Lần Thứ 2

--- NguyenKyKhoi_65131516_thigk2/src/thigk2/nguyenkykhoi/MainSanPham.java
+++ NguyenKyKhoi_65131516_thigk2/src/thigk2/nguyenkykhoi/MainSanPham.java
@@ -3,16 +3,24 @@ package thigk2.nguyenkykhoi;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//1. tạo danh sách ArrayList dùng foreach kết hợp to String để xuất
+
 public class MainSanPham {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Nhập số lượng sản phẩm trong danh sách : ");
-		int n = sc.nextInt();
-		sc.nextLine();
-		ArrayList<SanPham> dsSanPham = new ArrayList<SanPham>(n);
+		// Tạo danh sách 3 sản phẩm
+		ArrayList<SanPham> dsSanPham = new ArrayList<SanPham>();
+
+		dsSanPham.add(new SanPham("SP001", "Laptop Gaming G15", "Điện tử", 22500000));
+		dsSanPham.add(new SanPham("SP002", "Chuột không dây M331", "Phụ kiện", 350000));
+		dsSanPham.add(new SanPham("SP003", "Bàn phím cơ K87", "Phụ kiện", 1200000));
+
+		for (var temp : dsSanPham) {
+			System.out.println(temp.toString());
+		}
 
 	}
 
