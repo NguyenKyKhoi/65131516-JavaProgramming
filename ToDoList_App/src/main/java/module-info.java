959bbb49c1be267f17587a6d3c212f2b1e3fa9be
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-05-15 21:37:10 +0700

    updated

--- ToDoList_App/src/main/java/module-info.java
+++ ToDoList_App/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module com.nguyenkykhoi.ToDoList_App {
-    requires javafx.controls;
-    exports com.nguyenkykhoi.ToDoList_App;
+	requires javafx.controls;
+
+	exports com.nguyenkykhoi.ToDoList_App;
+
 }
