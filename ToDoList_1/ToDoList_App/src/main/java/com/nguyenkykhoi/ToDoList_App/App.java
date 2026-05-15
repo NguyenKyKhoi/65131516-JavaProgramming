372d93849387f79c0114684730c27855519cc95e
Nguyen Ky Khoi <nguyenkykhoi2603@gmail.com>
2026-05-15 20:40:37 +0700

    updated

--- ToDoList_1/ToDoList_App/src/main/java/com/nguyenkykhoi/ToDoList_App/App.java
+++ ToDoList_1/ToDoList_App/src/main/java/com/nguyenkykhoi/ToDoList_App/App.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -15,29 +16,45 @@ public class App extends Application {
 	public void start(Stage primaryStage) {
 
 		GridPane grid = new GridPane();
-		grid.setHgap(5);
+		grid.setHgap(20);
 		grid.setVgap(5);
 
-		Scene scene = new Scene(grid, 400, 300);
+		Scene scene = new Scene(grid, 550, 450);
 
 		// Tạo nút thêm
-		Button btn = new Button("Click me");
+		Button button = new Button("Add Task");
 
 		// Tạo chỗ nhập
 		TextField TFinput = new TextField();
 
 		// cho nút và chỗ nhập vào 1 chỗ ngang hàng
 		HBox hbox = new HBox(5);
-		hbox.getChildren().addAll(btn, TFinput);
+		hbox.getChildren().addAll(button, TFinput);
 
 		// Tạo danh sách
 		ListView<CheckBox> List = new ListView<>();
 
+		// danh sách đã xong
+		ListView<CheckBox> listDone = new ListView<>();
+
+		Text tDone = new Text("Task Done");
+
 		// Thêm sự kiện khi nhấn
-		btn.setOnAction(e -> {
+		button.setOnAction(e -> {
 			String text = TFinput.getText();
 			if (!text.isEmpty()) {
 				CheckBox checkBox = new CheckBox(text);
+
+				checkBox.setOnAction(ecb -> {
+					if (checkBox.isSelected()) {
+						List.getItems().remove(checkBox);
+						listDone.getItems().add(checkBox);
+					} else {
+						listDone.getItems().remove(checkBox);
+						List.getItems().add(checkBox);
+					}
+				});
+
 				List.getItems().add(checkBox);
 				TFinput.clear();
 			}
@@ -45,7 +62,10 @@ public class App extends Application {
 
 		grid.add(hbox, 0, 0);
 		grid.add(List, 0, 1);
+		grid.add(tDone, 1, 0);
+		grid.add(listDone, 1, 1);
 
+		primaryStage.setTitle("To Do List App");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
